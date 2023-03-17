c1be93fae898d4e51152e2aaa2886e12d5ee3e6b
Pietro Amato <pieamato98@gmail.com>
2023-03-17 17:47:11 +0100

    ex20-js solution

---
+++ javascript/ex20-javascript/exercise-6.js
@@ -0,0 +1,41 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(area){
+    if (area instanceof Square) {
+      return (area.side*area.side)
+    } else if (area instanceof Rectangle) {
+      return (area.height*area.width)/2
+    } else if (area instanceof Circle) {
+      return (area.radius*area.radius)*3.14
+    } else {
+      console.log("another geometric figure")
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
+
