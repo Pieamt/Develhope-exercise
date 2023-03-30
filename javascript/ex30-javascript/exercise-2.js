8837dab10779e3e030565410031f6b04d689e793
Pietro Amato <pieamato98@gmail.com>
2023-03-30 11:04:21 +0200

    ex30-fix

--- javascript/ex30-javascript/exercise-2.js
+++ javascript/ex30-javascript/exercise-2.js
@@ -11,9 +11,10 @@ class Person {
   }
 
   static fromJson(value) {
-    return JSON.parse(value)
+    let {id, firstName, lastName, age} = JSON.parse(value)
+    return new Person(id, firstName, lastName, age)
   }
-}
+} 
 
 const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
 const developer = Person.fromJson(json);
