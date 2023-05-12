4ff2118e6792853d24710f79f3d308f8032d0781
Pietro Amato <pieamato98@gmail.com>
2023-05-12 12:36:22 +0200

    fix-ex-30

--- react/ex-30-contextlanguage/src/DisplayLanguages.jsx
+++ react/ex-30-contextlanguage/src/DisplayLanguages.jsx
@@ -1,29 +1,17 @@
 import React from "react";
-import { LanguageContext } from "./LanguageContext";
+// import { LanguageContext } from "./LanguageContext";
 
-const SayHello = {
-  en: {
-    SAY_HELLO: "hello world",
-  },
-  it: {
-    SAY_HELLO: "ciao mondo",
-  },
-};
+// const SayHello = {
+//   en: {
+//     SAY_HELLO: "hello world",
+//   },
+//   it: {
+//     SAY_HELLO: "ciao mondo",
+//   },
+// };
 
 export default class DisplayLanguages extends React.Component {
   render() {
-    return (
-      <>
-        <LanguageContext.Consumer>
-          {(language) => {
-            return (
-              <div>
-                <h1>{SayHello[language].SAY_HELLO}</h1>
-              </div>
-            )
-          }}
-        </LanguageContext.Consumer>
-      </>
-    );
+    return <h1>Hello world</h1>
   }
 }
